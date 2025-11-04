33862e333d837ac014414600786d788908ad142b
Santiago Gutierrez <santigutierrezsch@gmail.com>
2025-11-04 14:07:23 -0600

    Finished notes on for loops / iterations

--- Iterations.java
+++ Iterations.java
@@ -4,10 +4,111 @@ import java.util.Scanner;
 /**
  * Examples and notes on iterations (Chapter 6)
  *
- * @author 
- * @version 
+ * @author mcallaghan
+ * @version 03nov2025
  */
 public class Iterations
 {
+    public static void whileExample()
+    {
+        /*
+         * while loop:
+         * 
+         *  evaluate a condition (i.e., boolean expression)
+         *      if true, executes the body of the loop and then reevaluates the condition
+         *      if false, skips the body and continue
+         */
+        int count = 1;          // initialization
+        
+        while (count <= 5)      // condition
+        {
+            System.out.println("The count is: " + count); // body
+            count++;            // update the loop variable
+            
+        }
+        
+        System.out.println("done.");
+        
+        // same loop with break keyword
+        count = 1;          // initialization
+        
+        while (true)      // condition
+        {
+            System.out.println("The count is: " + count); // body
+            count++;            // update the loop variable
+            if(count > 5)
+            {
+                break;
+            }
+            
+        }
+        
+        System.out.println("done.");
+        
+        /*
+         * infinite loop example
+         * 
+         * The count of 50 will never be reached!  Counting up from 1 by twos.
+         */
+        
+        // count = 1;
+        
+        // while(count != 50)
+        // {
+            // System.out.println(count);
+            // count += 2;
+        // }
+        // System.out.println("done.");
+    }
     
+    public static void forExample()
+    {
+        /*
+         * for loop:
+         * 
+         * Three parts of the for statement:
+         *          1. initialization - executed once
+         *          2. condition - boolean expression evaluated at the start of each loop iteration
+         *          3. update the loop variable - executed at the end of each loop iteration
+         *   
+         * 
+         */
+        final int LIMIT = 5;
+        int number;
+        for (number=1;                       // initialization
+            number <= LIMIT;                     // condition
+            number++)                            // update of the loop variable
+        {
+            System.out.println(number);          // body
+        }
+        
+        System.out.println("done");
+        
+        /*
+         * Variables declared within the for statement are scoped to the for statement and its body
+         *          Can't use the variable outside of the for loop unless initialized outside of the loop
+         */
+        
+        System.out.println(number);
+
+    }
+    
+    public static void offByOne()
+    {
+        /*
+         * The infamour off-by-one error is common with for loops, executing one too many or one too few times.
+         * 
+         * Carefully ask:
+         *                  Should the initial value start at 0 or 1
+         *                  Should the condition be inclusive or exclusive ( <= or <)
+         *                  
+         * By convention, for simple for loops, we start at 0 and use the < operator.
+         */
+        
+        // we want to print five *
+        for (int i=0; i<5; i++)
+        {
+            System.out.println("*");
+        }
+    }
 }
